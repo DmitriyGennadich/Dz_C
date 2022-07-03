2e5768a275eb4e9624d671226c3ff57dae296784
Dmitriy <fpmt2010@rambler.ru>
2022-07-03 21:07:07 +0300

    add Dz_3

---
+++ Dz_3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace seminar2
+{
+    class homework
+    //     Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+    // и проверяет, является ли этот день выходным.
+
+    // 6 -> да
+    // 7 -> да
+    // 1 -> нет
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер дня недели: ");
+            int number = (Convert.ToInt32(Console.ReadLine())) % 7;
+
+            if (number < 5)
+            {
+                Console.WriteLine("Будни");
+            }
+            else
+            {
+                Console.WriteLine("Выходной");
+            }
+
+        }
+    }
+}
