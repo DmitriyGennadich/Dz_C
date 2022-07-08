3a3216c9e4ad5380eb9e7d208442d08adaf23dca
Dmitriy <fpmt2010@rambler.ru>
2022-07-08 15:19:35 +0300

    add Ex

---
+++ Ex_2_1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace seminar3
+
+{
+    class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());     //Читаем число с консоли
+           
+            for (int i = 1; i <= a; i++)         // перебор массива с помощью цикла
+            {
+                PrintSquare(i);
+            }
+        }
+
+
+        public static void PrintSquare(int n)  // Объявление функции
+        {
+            Console.WriteLine(n * n);
+        }
+    }
+}
